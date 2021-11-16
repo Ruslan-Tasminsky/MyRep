78f228093db991e6e4494686be806eb995001f16
Ruslan Tasminsky <developed.acc@gmail.com>
2021-11-16 15:10:47 +0200

    product

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,27 +2,35 @@
 
 namespace app\controllers;
 
+use app\models\BreadcrumbsModel;
 use app\models\ProductModel;
 
 class ProductController extends AppController
 {
     public function viewAction()
     {
+        //Стиль карточки продуктов
         $alias = $this->route["alias"];
         $product = \R::findOne("product", "alias = ? AND status = '1'", [$alias]);
         if (!$product) {
             throw new \Exception('Страница не найдена', 404);
         }
+        //Хлебные крошки
+        $breadcrumbs = BreadcrumbsModel::getBreadcrumbs($product->category_id, $product->title);
+        //Связаные товары
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
         $p_model = new ProductModel();
         $p_model->setRecentlyViewed($product->id);
         $r_viewed = $p_model->getRecentlyViewed();
         $recentlyViewed = null;
-        if($r_viewed) {
+        if ($r_viewed) {
             $recentlyViewed = \R::find("product", "id IN (" . \R::genSlots($r_viewed) . ") LIMIT 3", $r_viewed);
         }
+        //галерея
         $gallery = \R::findAll("gallery", "product_id = ?", [$product->id]);
+        //Модификации
+        $mods = \R::findAll("modification", "product_id = ?", [$product->id]);
         $this->setMeta($product->title, $product->descripton, $product->keywords);
-        $this->set(compact("product", "related", "gallery", "recentlyViewed"));
+        $this->set(compact("product", "related", "gallery", "recentlyViewed", "breadcrumbs", "mods"));
     }
 }
--- app/views/Product/view.php
+++ app/views/Product/view.php
@@ -3,8 +3,7 @@
 		<div class="container">
 			<div class="breadcrumbs-main">
 				<ol class="breadcrumb">
-					<li><a href="index.html">Home</a></li>
-					<li class="active">Single</li>
+					<?= $breadcrumbs; ?>
 				</ol>
 			</div>
 		</div>
@@ -52,7 +51,7 @@
 									<div class="clearfix"> </div>
 								</div>
 
-								<h5 class="item_price"><?= $curr["symbol_left"] ?><?= (int) $product->price * $curr["value"]; ?><?= $curr["symbol_right"] ?></h5>
+								<h5 class="item_price" id="base-price" data-base="<?= $product->price * $curr['value']; ?>"><?= $curr['symbol_left']; ?><?= $product->price * $curr['value']; ?><?= $curr['symbol_right']; ?></h5>
 								<?php if ($product->old_price) : ?>
 									<del><?= $curr["symbol_left"] ?><?= (int) ($product->old_price * $curr["value"]); ?><?= $curr["symbol_right"] ?></del>
 								<?php endif; ?>
@@ -61,19 +60,12 @@
 									<ul>
 										<li>Color
 											<select>
-												<option>Silver</option>
-												<option>Black</option>
-												<option>Dark Black</option>
-												<option>Red</option>
+												<option>Base</option>
+												<?php foreach ($mods as $mod) : ?>
+													<option data-title="<?= $mod->title; ?>" data-price="<?= $mod->price * $curr['value']; ?>" value="<?= $mod->id; ?>"><?= $mod->title; ?></option>
+												<?php endforeach; ?>
 											</select>
 										</li>
-										<li class="size-in">Size<select>
-												<option>Large</option>
-												<option>Medium</option>
-												<option>small</option>
-												<option>Large</option>
-												<option>small</option>
-											</select></li>
 										<div class="clearfix"> </div>
 									</ul>
 								</div>
