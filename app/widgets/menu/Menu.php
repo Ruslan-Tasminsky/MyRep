2f08fb0887a8777ce368c2dff24aa2a2556a61c6
Ruslan Tasminsky <developed.acc@gmail.com>
2022-01-13 13:20:26 +0200

    category

--- app/widgets/menu/Menu.php
+++ app/widgets/menu/Menu.php
@@ -6,7 +6,8 @@ use ishop\App;
 use ishop\Cache;
 use RedUNIT\Base\Threeway;
 
-class Menu{
+class Menu
+{
 
     protected $data;
     protected $tree;
@@ -20,75 +21,81 @@ class Menu{
     protected $attrs = [];
     protected $prepend = '';
 
-    public function __construct($options = []){
+    public function __construct($options = [])
+    {
         $this->tpl = __DIR__ . '/menu_tpl/menu.php';
         $this->getOptions($options);
         $this->run();
     }
 
-    protected function getOptions($options){
-        foreach($options as $k => $v){
-            if(property_exists($this, $k)){
+    protected function getOptions($options)
+    {
+        foreach ($options as $k => $v) {
+            if (property_exists($this, $k)) {
                 $this->$k = $v;
             }
         }
     }
 
-    protected function run(){
+    protected function run()
+    {
         $cache = Cache::instance();
         $this->menuHtml = $cache->get($this->cacheKey);
-        if(!$this->menuHtml){
+        if (!$this->menuHtml) {
             $this->data = App::$app->getProperty('cats');
-            if(!$this->data){
+            if (!$this->data) {
                 $this->data = $cats = \R::getAssoc("SELECT * FROM {$this->table}");
             }
             $this->tree = $this->getTree();
             $this->menuHtml = $this->getMenuHtml($this->tree);
-            if($this->cache){
+            if ($this->cache) {
                 $cache->set($this->cacheKey, $this->menuHtml, $this->cache);
             }
         }
         $this->output();
     }
 
-    protected function output(){
+    protected function output()
+    {
         $attrs = '';
-        if(!empty($this->attrs)){
-            foreach($this->attrs as $k => $v){
+        if (!empty($this->attrs)) {
+            foreach ($this->attrs as $k => $v) {
                 $attrs .= " $k='$v' ";
             }
         }
         echo "<{$this->container} class='{$this->class}' $attrs>";
-            echo $this->prepend;
-            echo $this->menuHtml;
+        echo $this->prepend;
+        echo $this->menuHtml;
         echo "</{$this->container}>";
     }
 
-    protected function getTree(){
+    protected function getTree()
+    {
         $tree = [];
         $data = $this->data;
-        foreach ($data as $id=>&$node) {
-            if (!$node['parent_id']){
+        foreach ($data as $id => &$node) {
+            if (!$node['parent_id']) {
                 $tree[$id] = &$node;
-            }else{
+            } else {
                 $data[$node['parent_id']]['childs'][$id] = &$node;
             }
         }
         return $tree;
     }
 
-    protected function getMenuHtml($tree, $tab = ''){
+    protected function getMenuHtml($tree, $tab = '')
+    {
         $str = '';
-        foreach($tree as $id => $category){
+        foreach ($tree as $id => $category) {
             $str .= $this->catToTemplate($category, $tab, $id);
         }
         return $str;
     }
 
-    protected function catToTemplate($category, $tab, $id){
+    protected function catToTemplate($category, $tab, $id)
+    {
         ob_start();
         require $this->tpl;
         return ob_get_clean();
     }
-
 }
